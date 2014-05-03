ac77347808eac3fcde0ed47b22076f4feecced57
Elizabeth Glavin <eglavin@gmail.com>
2014-05-02 20:26:16 -0400

    spree styling

---
+++ app/overrides/cart.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar.html.erb", 
+                     :name => "cart", 
+                     :replace_contents => "#link-to-cart", 
+                     :text => "")
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DailyJarJuice1::Application.config.secret_key_base = '8120b38ab6bf9b491f8c18bd2a0d9369fa3c5542f1a0ef218f850c3e4157d41416cb160a942f1aaeaf54535a028257d89dc13af81681867fa296dfb8b2c071e4'
