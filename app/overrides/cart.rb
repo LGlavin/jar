4e0d49c0fe4eddcde75a6e152515db13deec131a
Elizabeth Glavin <eglavin@gmail.com>
2014-05-03 13:08:02 -0400

    cart

--- app/overrides/cart.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar.html.erb", 
-                     :name => "cart", 
-                     :replace_contents => "#link-to-cart", 
-                     :text => "")
---
+++ app/overrides/h1.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => "spree/orders/edit", 
+                     :name => "h1", 
+                     :replace_contents => "h1", 
+                     :text => "Cart")
